270f5f5ccdd2cb1b884f5fe599b3484f1eb37684
BinaraDulsara <binaradulsara021@gmil.com>
2025-03-05 15:16:54 +0530

    Made some Little Changes to a SOUT

--- src/main/java/lk/dulz/task1fx/HelloApplication.java
+++ src/main/java/lk/dulz/task1fx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Calculator!");
+        stage.setTitle("A BOX CALCULATOR!");
         stage.setScene(scene);
         stage.show();
     }
@@ -21,5 +21,6 @@ public class HelloApplication extends Application {
         launch();
 
         System.out.println("Dulsara");
+        System.out.println("DE SLVA");
     }
 }
